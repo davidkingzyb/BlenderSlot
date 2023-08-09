1cc743dcbf76bba613b97d24ed815c75be5e1c0c
DKZ <davidkingzyb@163.com>
2023-08-09 11:27:13 +0800

    Add files via upload

---
+++ slot/to_tris.py
@@ -0,0 +1,9 @@
+import bpy
+
+obj = bpy.context.active_object
+
+bpy.ops.object.mode_set(mode='EDIT')
+bpy.ops.mesh.select_all(action='SELECT')
+bpy.ops.mesh.quads_convert_to_tris()
+bpy.ops.mesh.normals_make_consistent(inside=False)
+bpy.ops.object.mode_set(mode='OBJECT')
